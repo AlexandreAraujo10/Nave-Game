da45f565f042fef9b2ec70f68e9573e7f7f908d5
AlexandreAraujo10 <xnd.araujo@gmail.com>
2023-11-23 18:51:56 -0300

    att

---
+++ script.js
@@ -0,0 +1,61 @@
+const botaoIniciar = document.getElementById("iniciar");
+const cenario = document.getElementById("cenario");
+const nave = document.getElementById("nave");
+
+const larguraCenario = cenario.offsetWidth;
+const alturaCenario = cenario.offsetHeight;
+
+const larguraNave = nave.offsetWidth;
+const alturaNave = nave.offsetHeight;
+
+const velocidadeNave = 15;
+
+let posicaoHorizontal = 0;
+let posicaoVertical = 0;
+let direcaoHorizontal =0;
+let direcaoVertical = 0;
+
+const teclaPressionada = (tecla) => {  /* esse comando, é p quando apertar a tecla */
+    if (tecla.key === "ArrowRight") { /* se a tecla for igual (ArrowRight) ceta p/ direita */
+        direcaoHorizontal = 1;
+    } else if (tecla.key === "ArrowLeft") {  /* se a tecla for igual (ArrowLeft) ceta p/ esquerda */
+        direcaoHorizontal = -1;
+    } else if (tecla.key ==="ArrowDown") {   /* se a tecla for igual (ArrowDown) ceta p/ baixo */
+        direcaoVertical = 1;
+    } else if (tecla.key === "ArrowUp") {   /* se a tecla for igual (ArrowDown) ceta p/ cima */
+        direcaoVertical = -1;
+    } 
+    
+}
+
+const teclaSolta = (tecla) => {   /* esse comando, é p quando soltar a tecla */
+    if (tecla.key === "ArrowRight" || tecla.key === "ArrowLeft") { /* se a tecla for igual (ArrowRight) ceta p/ direita */
+        direcaoHorizontal = 0;
+    } 
+    } else if (tecla.key ==="ArrowDown" || tecla.key === "ArrowUp") {   /* se a tecla for igual (ArrowDown) ceta p/ baixo */
+        direcaoVertical = 0;
+} 
+
+const moveNave = () => {   /* esse comando é p/ mover a nave */
+    posicaoHorizontal += direcaoHorizontal * velocidadeNave;  /* posic.horiz. recebe o valor de direc.horiz. vezez a velocidade */
+    posicaoVertical += direcaoVertical * velocidadeNave;    /* posic.vert. recebe o valor de direc.vert. vezez a velocidade */
+    if (posicaoHorizontal < 0) {   /* movimento horizontal */
+        posicaoHorizontal = 0;
+    } else if (posicaoHorizontal + larguraNave > larguraCenario) {  /* se a pos.horiz. + a larg.Nave for maior que larg.Cenar. */
+        posicaoHorizontal = larguraCenario - larguraNave;  /* então a pos.horiz.é igual a larg.Cenar. menos larg.Nave (nave não sai do cenário)  */
+    }
+    if (posicaoVertical < 0) {   /* movimento vertical */
+        posicaoVertical = 0;
+    } else if (posicaoVertical + alturaNave > larguraCenario) {  /* se a pos.vert. + a alt.Nave for maior que alt.Cenar. */
+        posicaoVertical = alturaCenario - larguraNave;  /* então a pos.vert.é igual a alt.Cenar. menos alt.Nave (nave não sai do cenário)  */
+    }
+    nave.style.left = posicaoHorizontal + "px";
+    nave.style.top = posicaoVertical + "px";
+}
+ 
+const iniciarJogo = () => {   /* evento p/ começar o jogo */
+    document.addEventListener("keydown", teclaPressionada);   /* (keydown)p/ tecla apertada */
+    document.addEventListener("keyup", teclaSolta);   /* (keyup)p/ tecla solta */
+    checaMoveNave = setInterval(moveNave, 50);
+    botaoIniciar.style.display = "none";
+}
